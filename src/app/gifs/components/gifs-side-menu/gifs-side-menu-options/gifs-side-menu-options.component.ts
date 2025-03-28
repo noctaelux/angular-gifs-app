49c1ef2f3ee81128e2ef5363c2f1701f334ee478
Oscar Calzada <noctaelux@gmail.com>
2025-03-28 14:43:50 -0600

    se avanza curso hasta el capítulo 6-73

---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './gifs-side-menu-options.component.html',
+})
+export class GifsSideMenuOptionsComponent {
+
+  menuOptions:MenuOption[] = [
+
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+
+  ]
+
+ }
